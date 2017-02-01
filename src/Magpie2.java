2ef661298dbf9b07f0ba4194dc6764c5330e40d5
Ollie Francis White <whiteof@dcsdk12.org>
2017-02-01 11:54:43 -0700

    activity2

--- src/Magpie2.java
+++ src/Magpie2.java
@@ -29,15 +29,16 @@ public class Magpie2 {
 	 *            the user statement
 	 * @return a response based on the rules given
 	 */
+	
 	public String getResponse(String statement) {
 		String response = "";
 		if (statement.indexOf("no") >= 0) {
 			response = "Why so negative?";
-		} else if (statement.indexOf("mother") >= 0
+		} else if (statement.indexOf("Mr Padjen ") >= 0
 				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0) {
-			response = "Tell me more about your family.";
+				|| statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0) {
+			response = "He sounds like a good teacher.";
 		} else {
 			response = getRandomResponse();
 		}
